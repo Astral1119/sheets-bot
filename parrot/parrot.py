26fb54a9054dc879d2e410c047d8b4bf84818b47
astral <astral@astral.cafe>
2025-06-02 17:10:54 -0600

    reworking parrot

--- parrot/parrot.py
+++ parrot/parrot.py
@@ -1,28 +1,51 @@
+"""
+parrot is a Discord bot cog that dynamically loads commands from a dictionary.
+"""
+
+command_list = {
+    "apis":
+        """
+        map: Returns an example of map function usage within Google Sheets
+        scan: Returns an example of scan function usage within Google Sheets
+        reduce: Returns an example of reduce function usage within Google Sheets
+        lambda: Returns an example of using lambda within Google Sheets
+        documentation: Returns how to use let to incorporate documentation in your formulas for Google Sheets
+        """,
+    "learngas":
+        """
+        [ExcelGoogleSheets](https://www.youtube.com/@ExcelGoogleSheets/search?query=apps%20script)
+        [Ben Collins](https://courses.benlcollins.com/p/apps-script-blastoff)
+        [Spencer Farris](https://www.youtube.com/playlist?list=PLmE9Sui7JoQGqOJvhxYRjOFUtr5kMWUtJ)
+        """,
+    "notable":
+        """
+        [Advanced Dropdown Setups](https://docs.google.com/spreadsheets/d/1OlRIXjoaUG5Owjd3t9hGfmV7G8EmAKffP7YVPdNGNH0/edit?usp=sharing)
+        [A History of Crash Bugs](https://docs.google.com/spreadsheets/d/107B_jSpObwxxYfL_HTBWZtB9cnMQDTraoirpaRUsNLc/edit?gid=582260365#gid=582260365)
+        [Community Practice Problems](https://docs.google.com/spreadsheets/d/1RZVTUJj_qzugq_WCd7rMjmjzKtUM72Jb5x0RGFAVNnk/edit?gid=890374412#gid=890374412)
+        """,
+}
+
 import discord
 from discord.ext import commands
 from discord import app_commands
-import json
-import os
 
 class Parrot(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.commands = []
 
-        for filename in os.listdir('parrot'):
-            if filename.endswith('.json'):
-                command_name = filename[:-5]
-                self.commands.append(command_name)
-                self.add_parrot_command(command_name)
+
+        for command_name, description in command_list.items():
+            self.commands.append(command_name)
+            self.add_parrot_command(command_name)
 
     def add_parrot_command(self, command_name):
-        with open(f'parrot/{command_name}.json') as f:
-            command_data = json.load(f)
+        command_data = command_list[command_name]
 
         @app_commands.allowed_installs(guilds=True, users=True)
         @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
         async def parrot(ctx):
-            description = "\n".join(f'**{label}**: {desc}' for label, desc in command_data.items())
+            description = command_data
             embed = discord.Embed(
                 title=command_name,
                 description=description,
@@ -30,7 +53,7 @@ class Parrot(commands.Cog):
             )
             await ctx.send(embed=embed)
 
-        parrot.__name__ = command_name  # Set correct function name
+        parrot.__name__ = command_name  # set correct function name
         self.client.add_command(commands.hybrid_command(name=command_name)(parrot))
 
 async def setup(client):
